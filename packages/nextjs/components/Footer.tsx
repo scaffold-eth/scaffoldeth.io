1ab58409c45522028a496fc094a23d47d25f8867
Carlos Sánchez <oceanrdn@gmail.com>
2023-08-04 11:52:43 +0200

    Remove dark mode logic + minimal footer (#10)

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -1,26 +1,20 @@
-import { HeartIcon } from "@heroicons/react/24/outline";
-import { SwitchTheme } from "~~/components/SwitchTheme";
+import { HeartIcon } from "@heroicons/react/24/solid";
 
 /**
  * Site footer
  */
 export const Footer = () => {
   return (
-    <div className="min-h-0 p-5 mb-11 lg:mb-0">
-      <div>
-        <div className="fixed flex justify-end items-center w-full z-10 p-4 bottom-0 left-0 pointer-events-none">
-          <SwitchTheme className="pointer-events-auto" />
-        </div>
-      </div>
+    <div className="min-h-0 p-5 bg-neutral">
       <div className="w-full">
         <ul className="menu menu-horizontal w-full">
-          <div className="flex justify-center items-center gap-2 text-sm w-full">
+          <div className="flex justify-center items-center gap-2 text-sm w-full text-white">
             <div>
               <a
-                href="https://github.com/scaffold-eth/se-2"
+                href="https://github.com/scaffold-eth/scaffold-eth-2"
                 target="_blank"
                 rel="noreferrer"
-                className="underline underline-offset-2"
+                className="underline underline-offset-2 hover:opacity-80 transition-opacity"
               >
                 Fork me
               </a>
@@ -32,7 +26,7 @@ export const Footer = () => {
                 href="https://buidlguidl.com/"
                 target="_blank"
                 rel="noreferrer"
-                className="underline underline-offset-2"
+                className="underline underline-offset-2 hover:opacity-80 transition-opacity"
               >
                 BuidlGuidl
               </a>
@@ -43,7 +37,7 @@ export const Footer = () => {
                 href="https://t.me/joinchat/KByvmRe5wkR-8F_zz6AjpA"
                 target="_blank"
                 rel="noreferrer"
-                className="underline underline-offset-2"
+                className="underline underline-offset-2 hover:opacity-80 transition-opacity"
               >
                 Support
               </a>
