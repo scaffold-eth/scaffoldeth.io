3761b5272ba5e7b7ee4a15fe9c50c7399146e6b2
Pablo Alayeto <55535804+Pabl0cks@users.noreply.github.com>
2023-08-08 16:13:27 +0200

    Add Hero background, update image assets, review copys and call to actions (#15)

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -6,44 +6,87 @@ import { CheckCircleIcon, DocumentDuplicateIcon } from "@heroicons/react/24/outl
 import { MetaHeader } from "~~/components/MetaHeader";
 
 const Home: NextPage = () => {
+  const [cloneCommandCopied, setCloneCommandCopied] = useState(false);
   const [npxCommandCopied, setNpxCommandCopied] = useState(false);
   return (
     <>
       <MetaHeader />
       {/* Hero section  */}
-      <div className="flex flex-col items-center py-8 gap-12 md:gap-20">
+      <div
+        className="flex flex-col items-center py-8 gap-12 md:gap-20"
+        style={{ backgroundImage: `url(/assets/heroPattern.svg)`, backgroundRepeat: "repeat" }}
+      >
         <div className="flex items-center gap-2">
           <div className="flex relative w-10 h-10">
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
           <p className="text-3xl m-0 font-medium mt-1">Scaffold-ETH 2</p>
         </div>
-        <div className="flex flex-col justify-center items-center gap-8 mb-14">
+        <div className="flex flex-col justify-center items-center gap-8 mb-14 px-4">
           <h1 className="text-center text-3xl lg:text-5xl max-w-md lg:max-w-2xl px-3 m-0">
             Everything you need to build dApps on Ethereum
           </h1>
           <p className="m-0 text-center max-w-xl px-3">
-            A modern, clean version of scaffold-eth with RainbowKit, NextJS and Typescript. Supports Hardhat and
+            A modern, clean version of Scaffold-ETH with RainbowKit, NextJS and Typescript. Supports Hardhat and
             Foundry.
           </p>
-          <CopyToClipboard
-            text={"npx create-eth@latest"}
-            onCopy={() => {
-              setNpxCommandCopied(true);
-              setTimeout(() => {
-                setNpxCommandCopied(false);
-              }, 800);
-            }}
-          >
-            <div className="max-w-sm flex border-2 border-gray-300 rounded-xl px-5 py-1 gap-2">
-              <p className="m-0">npx create-eth@latest</p>
-              {npxCommandCopied ? (
-                <CheckCircleIcon className="text-xl font-normal h-6 w-4 cursor-pointer" aria-hidden="true" />
-              ) : (
-                <DocumentDuplicateIcon className="text-xl font-normal h-6 w-4 cursor-pointer" aria-hidden="true" />
-              )}
+          <div className="flex flex-col gap-5 items-center mb-2">
+            <CopyToClipboard
+              text={"git clone https://github.com/scaffold-eth/scaffold-eth-2.git"}
+              onCopy={() => {
+                setCloneCommandCopied(true);
+                setTimeout(() => {
+                  setCloneCommandCopied(false);
+                }, 800);
+              }}
+            >
+              <div className="mx-2 flex border-2 border-gray-300 rounded-xl px-3 sm:px-5 py-1 gap-2">
+                <p className="m-0 text-center text-sm sm:text-base">
+                  git clone https://github.com/scaffold-eth/scaffold-eth-2.git
+                </p>
+                {cloneCommandCopied ? (
+                  <CheckCircleIcon
+                    className="text-xl font-normal h-6 w-4 flex-shrink-0 cursor-pointer"
+                    aria-hidden="true"
+                  />
+                ) : (
+                  <DocumentDuplicateIcon
+                    className="text-xl font-normal h-6 w-4 flex-shrink-0 cursor-pointer"
+                    aria-hidden="true"
+                  />
+                )}
+              </div>
+            </CopyToClipboard>
+            <div className="divider px-6 sm:px-20 m-0">OR</div>
+            <div className="flex items-center gap-2 mx-2">
+              <CopyToClipboard
+                text={"npx create-eth@latest"}
+                onCopy={() => {
+                  setNpxCommandCopied(true);
+                  setTimeout(() => {
+                    setNpxCommandCopied(false);
+                  }, 800);
+                }}
+              >
+                <div className="max-w-sm flex border-2 border-gray-300 rounded-xl px-3 sm:px-5 py-1 gap-2">
+                  <p className="m-0 text-center text-sm sm:text-base">npx create-eth@latest</p>
+                  {npxCommandCopied ? (
+                    <CheckCircleIcon
+                      className="text-xl font-normal h-6 w-4 cursor-pointer flex-shrink-0"
+                      aria-hidden="true"
+                    />
+                  ) : (
+                    <DocumentDuplicateIcon
+                      className="text-xl font-normal h-6 w-4 cursor-pointer flex-shrink-0"
+                      aria-hidden="true"
+                    />
+                  )}
+                </div>
+              </CopyToClipboard>
+              <div className="badge badge-neutral">Beta</div>
             </div>
-          </CopyToClipboard>
+          </div>
+
           <div className="flex flex-wrap gap-4 items-center justify-center">
             <a
               href="https://scaffold-eth-2-docs.vercel.app/"
@@ -51,7 +94,7 @@ const Home: NextPage = () => {
               className="btn btn-outline btn-sm px-5 h-10 bg-base-100 normal-case font-normal text-lg"
               rel="noreferrer"
             >
-              View the Docs
+              Docs
             </a>
             <a
               href="https://github.com/scaffold-eth/scaffold-eth-2"
@@ -59,7 +102,7 @@ const Home: NextPage = () => {
               className="btn btn-outline btn-sm px-5 h-10 bg-neutral text-white normal-case font-normal text-lg flex items-center gap-2"
               rel="noreferrer"
             >
-              <span>Start building</span>
+              <span>Github</span>
               <Image src="/assets/ghIcon.png" alt="github icon" height={25} width={25} />
             </a>
           </div>
@@ -84,7 +127,7 @@ const Home: NextPage = () => {
             </p>
           </div>
           <div className="max-w-[400px] lg:max-w-none">
-            <Image src="/assets/seDebugContracts.png" alt="Debug contracts" width={1200} height={1200} />
+            <Image src="/assets/se2DebugContracts.png" alt="Debug contracts" width={1400} height={1400} />
           </div>
         </div>
       </div>
@@ -107,8 +150,8 @@ const Home: NextPage = () => {
                 Common web3 components in tailwind and daisy UI
               </h2>
               <p className="m-auto text-center lg:text-left lg:mx-0 max-w-[300px] lg:max-w-none lg:w-3/4">
-                Accelerate your dapp development with Scaffold-eth 2 pre-built components for common web3 use cases. Use
-                tailwind and daisy UI to style your dapp and give it a modern and appealing design.
+                Accelerate your dapp development using our pre-built components for common web3 use cases. Tailwind and
+                daisyUI to style your dapp and give it a modern and appealing design.
               </p>
             </div>
           </div>
@@ -120,8 +163,8 @@ const Home: NextPage = () => {
         <div className="flex flex-col justify-center items-center bg-base-200 w-4/5 max-w-3xl lg:w-5/12 rounded-3xl shadow-lg shadow-primary p-10 gap-4 -mb-12">
           <p className="text-center text-3xl font-medium m-0">Buidl in Community</p>
           <p className="text-center m-0">
-            Open source forkable Ethereum dev stack built, mantained and supported by BuidlGuidl community, with over
-            800 active builders.
+            You can build and learn together with the BuidlGuidl community, joining over 800 members in creating
+            products, prototypes, and tutorials to enrich the web3 ecosystem.
           </p>
           <a
             href="https://buidlguidl.com"
@@ -129,7 +172,7 @@ const Home: NextPage = () => {
             className="btn btn-outline btn-sm px-5 h-10 bg-base-100 normal-case font-normal text-lg"
             rel="noreferrer"
           >
-            View BuidlGuidl
+            Learn More
           </a>
         </div>
       </div>
@@ -147,12 +190,12 @@ const Home: NextPage = () => {
               Built-in Block Explorer to check transaction data easily during your tests
             </h2>
             <p className="m-auto text-center lg:text-left lg:mx-0 max-w-[300px] lg:max-w-none lg:w-3/4">
-              Review transaction data to make sure everything is working as expected. With Scaffold-ETH 2 built-in Block
-              Explorer you can check the transactions of your dapp during your local tests.
+              Review transaction data from your local tests to make sure everything is working as expected. With our
+              built-in Block Explorer, you can check the transaction details from your dapp while tinkering with it.
             </p>
           </div>
           <div className="max-w-[400px] lg:max-w-none">
-            <Image src="/assets/blockExplorer.png" alt="Block Explorer" width={1400} height={1400} />
+            <Image src="/assets/se2BlockExplorer.png" alt="Block Explorer" width={1400} height={1400} />
           </div>
         </div>
       </div>
