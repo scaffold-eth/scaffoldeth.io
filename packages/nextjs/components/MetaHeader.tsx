d2bd5a5d3a20c5c23b448f99d7c024a285d5da51
tokodev <pablo@asdloop.com>
2023-08-18 12:06:26 +0200

    Revert hardcoded production domain and add hardcoded twitterThumbnail with different size ratio

--- packages/nextjs/components/MetaHeader.tsx
+++ packages/nextjs/components/MetaHeader.tsx
@@ -11,9 +11,7 @@ type MetaHeaderProps = {
 
 // Images must have an absolute path to work properly on Twitter.
 // We try to get it dynamically from Vercel, but we default to relative path.
-const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/`
-  : "https://scaffoldeth.io/";
+const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/` : "/";
 
 export const MetaHeader = ({
   title = "Scaffold-ETH 2 - Open source toolkit to build dApps on Ethereum",
@@ -43,7 +41,7 @@ export const MetaHeader = ({
       {image && (
         <>
           <meta property="og:image" content={imageUrl} />
-          <meta name="twitter:image" content={imageUrl} />
+          <meta name="twitter:image" content="twitterThumbnail.jpg" />
         </>
       )}
       {twitterCard && <meta name="twitter:card" content={twitterCard} />}
