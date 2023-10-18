a33ee0883cc90e5b5605136ce565bdee749b323a
Shiv Bhonde <shivbhonde04@gmail.com>
2023-10-18 11:43:28 +0530

    hooks section: use dark color for values in hook

--- packages/nextjs/components/HooksExample.tsx
+++ packages/nextjs/components/HooksExample.tsx
@@ -1,15 +1,13 @@
 import { ReactNode } from "react";
 
-const Red = ({ children }: { children: ReactNode }) => {
-  return <span className="text-[#FF5962]">{children}</span>;
-};
-const Cyan = ({ children }: { children: ReactNode }) => {
-  return <span className="text-[#00B0A6]">{children}</span>;
-};
+const Red = ({ children }: { children: ReactNode }) => <span className="text-[#FF5962]">{children}</span>;
 
-const Purple = ({ children }: { children: ReactNode }) => {
-  return <span className="text-[#7B61FF]">{children}</span>;
-};
+const Cyan = ({ children, dark }: { children: ReactNode; dark?: boolean }) => (
+  <span className={`${dark ? "text-[#00968D]" : "text-[#00B0A6]"}`}>{children}</span>
+);
+
+const Purple = ({ children }: { children: ReactNode }) => <span className="text-[#7B61FF]">{children}</span>;
+
 export const HooksExample = () => {
   return (
     <div className="w-full flex-grow  bg-white rounded-2xl p-6 drop-shadow-[0px_0px_10px_rgba(139,178,241,0.50)]">
@@ -28,13 +26,13 @@ export const HooksExample = () => {
           <Purple>useScaffoldContractWrite</Purple>({"{"}
           <br />
           <span className="inline-block ml-4">
-            <Cyan>contractName</Cyan>: <Cyan> &quot;YourContract&quot;</Cyan>,
+            <Cyan>contractName</Cyan>: <Cyan dark>&quot;YourContract&quot;</Cyan>,
             <br />
-            <Cyan>functionName</Cyan>: <Cyan> &quot;setPurpose&quot;</Cyan>,
+            <Cyan>functionName</Cyan>: <Cyan dark>&quot;setPurpose&quot;</Cyan>,
             <br />
-            <Cyan>args</Cyan>: [<Cyan>&quot;The value to set&quot;</Cyan>],
+            <Cyan>args</Cyan>: [<Cyan dark>&quot;The value to set&quot;</Cyan>],
             <br />
-            <Cyan>blockConfirmation</Cyan>: <Cyan>1</Cyan>,
+            <Cyan>blockConfirmation</Cyan>: <Cyan dark>1</Cyan>,
             <br />
             <Purple>onBlockConfirmation</Purple>: (txnReceipt) =&gt; {"{"}
             <br />
@@ -54,7 +52,7 @@ export const HooksExample = () => {
           the hook. Here&apos;s an example usage:
         </p>
         <p className="my-3">
-          <Red>{"<"}button</Red> className=&quot;<Cyan>btn btn-primary</Cyan>&quot; onClick<Cyan>=</Cyan>
+          <Red>{"<"}button</Red> className<Cyan>=&quot;btn btn-primary&quot;</Cyan> onClick<Cyan>=</Cyan>
           {"{"}() <Cyan>=&gt;</Cyan> writeAsync(){"}"}
           <Red>{">"}</Red>
           <br />
