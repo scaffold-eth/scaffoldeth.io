4afb04127b2a2df3d43be6723cae6574cc64b062
Carlos Sánchez <oceanrdn@gmail.com>
2024-11-21 17:46:04 +0100

    Remove clone command + Highlight npx command (#41)

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -8,7 +8,6 @@ import { MetaHeader } from "~~/components/MetaHeader";
 import TrackedLink from "~~/components/TrackedLink";
 
 const Home: NextPage = () => {
-  const [cloneCommandCopied, setCloneCommandCopied] = useState(false);
   const [npxCommandCopied, setNpxCommandCopied] = useState(false);
   const [extensionCommandCopied, setExtensionCommandCopied] = useState(false);
   return (
@@ -34,33 +33,6 @@ const Home: NextPage = () => {
             Foundry.
           </p>
           <div className="flex flex-col gap-5 items-center mb-2">
-            <CopyToClipboard
-              text={"git clone https://github.com/scaffold-eth/scaffold-eth-2.git"}
-              onCopy={() => {
-                setCloneCommandCopied(true);
-                setTimeout(() => {
-                  setCloneCommandCopied(false);
-                }, 800);
-              }}
-            >
-              <div className="mx-2 flex border-2 border-gray-300 rounded-xl px-3 sm:px-5 py-1 gap-2">
-                <p className="m-0 text-center text-sm sm:text-base">
-                  git clone https://github.com/scaffold-eth/scaffold-eth-2.git
-                </p>
-                {cloneCommandCopied ? (
-                  <CheckCircleIcon
-                    className="text-xl font-normal h-6 w-4 flex-shrink-0 cursor-pointer"
-                    aria-hidden="true"
-                  />
-                ) : (
-                  <DocumentDuplicateIcon
-                    className="text-xl font-normal h-6 w-4 flex-shrink-0 cursor-pointer"
-                    aria-hidden="true"
-                  />
-                )}
-              </div>
-            </CopyToClipboard>
-            <div className="divider px-6 sm:px-20 m-0">OR</div>
             <div className="flex items-center gap-2 mx-2">
               <CopyToClipboard
                 text={"npx create-eth@latest"}
@@ -71,8 +43,8 @@ const Home: NextPage = () => {
                   }, 800);
                 }}
               >
-                <div className="max-w-sm flex border-2 border-gray-300 rounded-xl px-3 sm:px-5 py-1 gap-2">
-                  <p className="m-0 text-center text-sm sm:text-base">npx create-eth@latest</p>
+                <div className="max-w-sm flex border-2 border-primary rounded-xl px-3 sm:px-5 py-1 gap-2">
+                  <p className="m-0 text-center text-sm sm:text-base font-bold">npx create-eth@latest</p>
                   {npxCommandCopied ? (
                     <CheckCircleIcon
                       className="text-xl font-normal h-6 w-4 cursor-pointer flex-shrink-0"
@@ -86,7 +58,6 @@ const Home: NextPage = () => {
                   )}
                 </div>
               </CopyToClipboard>
-              <div className="badge badge-warning">Beta</div>
             </div>
           </div>
 
@@ -218,7 +189,7 @@ const Home: NextPage = () => {
                   }, 800);
                 }}
               >
-                <div className="flex items-center justify-between border-2 border-gray-300 rounded-xl px-3 py-2 text-xs sm:text-sm">
+                <div className="flex items-center justify-between border-2 border-primary rounded-xl px-3 py-2 text-xs sm:text-sm">
                   <p className="m-0 mr-2">npx create-eth@latest -e gitHubUsername/repoName</p>
                   {extensionCommandCopied ? (
                     <CheckCircleIcon
