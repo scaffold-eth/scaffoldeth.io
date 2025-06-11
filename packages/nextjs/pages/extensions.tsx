75fa0b67e66bd220a863d9ddbbd55f698049312a
Shiv Bhonde | shivbhonde.eth <shivbhonde04@gmail.com>
2025-06-11 14:28:59 +0530

    add delegation extension (#50)

--- packages/nextjs/pages/extensions.tsx
+++ packages/nextjs/pages/extensions.tsx
@@ -13,8 +13,8 @@ type Extension = {
   description: string;
   github: string;
   installCommand: string;
-  builder: string;
-  coBuilders: string[];
+  builder?: string;
+  coBuilders?: string[];
   youtube?: string;
 };
 
@@ -28,6 +28,15 @@ type CuratedExtensionResponse = {
   name?: string; // human redable name, if not present we default to branch or extensionFlagValue on UI
 }[];
 
+// TODO: Remove this and find a better way to allow extension submission from different orgs.
+const metamaskExtension: Extension = {
+  name: "Delegation Toolkit Extension",
+  description:
+    "The MetaMask Delegation Toolkit is a Viem-based collection of tools for integrating embedded smart accounts, known as `MetaMaskSmartAccount`, into dapps. Developers can create and manage delegator accounts that delegate specific permissions, such as spending limits or time-based access, to other accounts. This extension demonstrates the end-to-end flow for initializing a MetaMask Smart Account, generating and signing a delegation, and redeeming the delegation according to [ERC-7710](https://eips.ethereum.org/EIPS/eip-7710) specifications. ",
+  github: "https://github.com/MetaMask/gator-extension",
+  installCommand: "npx create-eth@latest -e metamask/gator-extension",
+};
+
 interface ExtensionsListProps {
   thirdPartyExtensions: Extension[];
   curatedExtensions: Extension[];
@@ -36,7 +45,7 @@ interface ExtensionsListProps {
 const ExtensionsList: NextPage<ExtensionsListProps> = ({ thirdPartyExtensions, curatedExtensions }) => {
   const [searchQuery, setSearchQuery] = useState("");
 
-  const allExtensions = [...curatedExtensions, ...thirdPartyExtensions];
+  const allExtensions = [...curatedExtensions, metamaskExtension, ...thirdPartyExtensions];
 
   const filteredExtensions = allExtensions.filter(extension => {
     if (searchQuery.length < 3) return true;
